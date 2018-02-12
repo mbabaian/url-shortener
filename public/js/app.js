473558dcea2a3ce1d379fc1c5543202e5b1b98eb
Monica <monica.babaian@gmail.com>
2018-02-11 18:51:10 -0600

    Changed html

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,5 @@
 // Angular application
-<<<<<<< HEAD
 //var angular = require('angular')
-=======
-var angular = require('angular')
-
->>>>>>> a9927db3ea6c90c24adfa551d07272cdddb29012
 var app = angular.module('shortUrlApp', []);
 
 app.controller('shortAppCtrl', ($scope) => {
